0b8f1d6583a281108e0ddbce04d4caf4e78d731a
Jacek TeMPOraL Złydach <temporal.pl@gmail.com>
2019-02-21 20:47:48 +0100

    Sketching code for generating line-item table.

--- lisp/invoicing.el
+++ lisp/invoicing.el
@@ -16,6 +16,8 @@
 ;;;; Dependencies
 (require 'cl-lib)
 (require 'org)
+(require 'org-table)
+(require 'dash-functional)
 
 ;; TODO code in general
 
@@ -58,6 +60,56 @@ or yet to be sent to the accounting people."
 
 
 ;; workbook generator code -- TODO separate out to another file
+
+(defun invoicing--agetter (key)
+  "Return an alist-getter for `KEY'.
+An alist-getter is a function that accepts an alist,
+and returns the value of `KEY'."
+  (-compose #'cdr (-partial #'assoc key)))
+
+(defvar invoicing--default-columns
+  `(("" "" ,(-const "#") ,(-const nil))
+    ("Tag" "Tag" ,#'car ,(-const nil))
+    ("Line item name" "LineItem" ,(invoicing--agetter :name) ,(-const nil))
+    ("Hours" "Hours" ,(invoicing--agetter :hours) ,(-const "TODO tblfm for total Hours"))
+    ("Rate" "Rate" ,(invoicing--agetter :rate) ,(-const "TODO tblfm for total Rate"))
+    ("Net" "Net" ,(-const nil) ,(-const `("TODO tblfm for row Net" "TODO tblfm for total Net")))
+    ;; -- insertion point for custom formulas; TODO mark it maybe?
+    ("Gross" "Gross" ,(-const nil) ,(-const `("TODO tblfm for row Gross" "TODO tblfm for total Gross")))))
+
+(defun invoicing--compute-additional-line-item-table-columns (params)
+  ;; TODO document
+  ;; should return a list of columns, each being a list of:
+  ;; - column name
+  ;; - column variable name (for formulas)
+  ;; - column getter - a function taking tag properties (with :hours being sum of hours in org mode), returning a value to put in the table
+  ;; - column formula generator - a function taking the list of columns in order, returning a list of tblfms to add
+  )
+
+(defun org-dblock-write:invoice-items (params)
+  "Generate an org mode table with invoice line items.
+TODO document `PARAMS'."
+  ;; Headline
+  (when (plist-get params :name)
+    (insert (format "#+NAME: %s\n" (plist-get params :name))))
+
+  (let ((additional-columns (invoicing--compute-additional-line-item-table-columns params))))
+
+  ;; Constant items: # column, Tag, Line item name, hours, rate, net, [], total
+  ;; Poland-relevant items: VAT %, VAT amount USD (if foreign), VAT amount PLN
+
+  ;; Contents
+  ;; TODO 1. collect data
+  ;; TODO 2. render via (orgtbl-to-orgtbl '(("foo" "bar" "baz") hline (1 2 3) (4 5 6)) nil)
+  ;; TODO 2a. remember to insert # at the beginning!
+  ;; TODO 2b. consider using ! and ^
+  ;; TODO 2c. consider using $ for currency conversion rate
+
+  ;; Generate formula row.
+  ;; TODO output formula row
+  )
+
+;;orgtbl-to-orgtbl
 
 
 ;; template code -- TODO separate out to another file
